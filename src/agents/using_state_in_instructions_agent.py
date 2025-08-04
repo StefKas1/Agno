a15e9db4206485698292b25bf32848f63251e7f3
StefKas1 <stefan.kasperzack@gmail.com>
2025-08-04 09:25:28 +0200

    Added state agents.

---
+++ src/agents/using_state_in_instructions_agent.py
@@ -0,0 +1,20 @@
+from textwrap import dedent
+
+from agno.agent import Agent
+from agno.models.openai import OpenAIChat
+from dotenv import load_dotenv
+
+agent = Agent(
+    model=OpenAIChat(id="gpt-4o-mini"),
+    # Initialize the session state with a variable
+    session_state={"user_name": "John"},
+    # You can use variables from the session state in the instructions
+    instructions="Users name is {user_name}",  # Don’t use f-string in instructions. Directly use the {key} syntax, Agno substitutes the values for you
+    show_tool_calls=True,
+    add_state_in_messages=True,
+    markdown=True,
+)
+
+if __name__ == "__main__":
+    load_dotenv()
+    agent.print_response("What is my name?", stream=True)
