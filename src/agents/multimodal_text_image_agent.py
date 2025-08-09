f5a40d9c42f53c0e704551d7d5af422f36a9689a
StefKas1 <stefan.kasperzack@gmail.com>
2025-08-09 09:28:33 +0200

    Added multimodal text-image agent.

---
+++ src/agents/multimodal_text_image_agent.py
@@ -0,0 +1,23 @@
+from agno.agent import Agent
+from agno.media import Image
+from agno.models.openai import OpenAIChat
+from agno.tools.duckduckgo import DuckDuckGoTools
+from dotenv import load_dotenv
+
+agent = Agent(
+    model=OpenAIChat(id="gpt-4o"),
+    tools=[DuckDuckGoTools()],
+    markdown=True,
+)
+
+if __name__ == "__main__":
+    load_dotenv()
+    agent.print_response(
+        "Tell me about this image and give me the latest news about it.",
+        images=[
+            Image(
+                url="https://upload.wikimedia.org/wikipedia/commons/0/0c/GoldenGateBridge-001.jpg"
+            )
+        ],
+        stream=True,
+    )
