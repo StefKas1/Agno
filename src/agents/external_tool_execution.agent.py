957ba0728a3d5eb48583fc1e1fff11439b0e9db8
StefKas1 <stefan.kasperzack@gmail.com>
2025-08-14 08:52:51 +0200

    Added external tool agent.

--- src/agents/external_tool_execution.agent.py
+++ src/agents/external_tool_execution.agent.py
@@ -33,7 +33,9 @@ if __name__ == "__main__":
     if run_response.is_paused:
         for tool in run_response.tools_awaiting_external_execution:
             if tool.tool_name == execute_shell_command.name:
-                print(f"Executing {tool.tool_name} with args {tool.tool_args} externally")
+                print(
+                    f"Executing {tool.tool_name} with args {tool.tool_args} externally"
+                )
 
                 # We execute the tool ourselves. You can execute any function or process here and use the tool_args as input.
                 result = execute_shell_command.entrypoint(**tool.tool_args)
