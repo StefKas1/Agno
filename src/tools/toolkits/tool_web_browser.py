31a881a326ffe714660c9e95e623446ddec59406
StefKas1 <stefan.kasperzack@gmail.com>
2025-09-14 10:06:41 +0200

    Added tool web browser.

---
+++ src/tools/toolkits/tool_web_browser.py
@@ -0,0 +1,17 @@
+from agno.agent import Agent
+from agno.models.google import Gemini
+from agno.tools.duckduckgo import DuckDuckGoTools
+from agno.tools.webbrowser import WebBrowserTools
+from dotenv import load_dotenv
+
+load_dotenv()
+
+agent = Agent(
+    tools=[WebBrowserTools(), DuckDuckGoTools()],
+    instructions=[
+        "Find related websites and pages using DuckDuckGo"
+        "Use web browser to open the site"
+    ],
+    markdown=True,
+)
+agent.print_response("Find an article explaining MCP and open it in the web browser.")
