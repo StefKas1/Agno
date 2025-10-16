9026bd630e3381aa274ed6b93c39ccf89e838b42
StefKas1 <stefan.kasperzack@gmail.com>
2025-10-16 08:21:58 +0200

    Added speech to text.

---
+++ src/multimodal/audio/speech_to_text.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from agno.agent import Agent
+from agno.run.agent import RunOutput
+from agno.tools.openai import OpenAITools
+from agno.utils.media import download_file
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Example 1: Transcription
+url = "https://agno-public.s3.amazonaws.com/demo_data/sample_conversation.wav"
+
+local_audio_path = Path("tmp/sample_conversation.wav")
+print(f"Downloading file to local path: {local_audio_path}")
+download_file(url, local_audio_path)
+
+transcription_agent = Agent(
+    tools=[OpenAITools(transcription_model="gpt-4o-transcribe")],
+    markdown=True,
+)
+transcription_agent.print_response(
+    f"Transcribe the audio file for this file: {local_audio_path}"
+)
