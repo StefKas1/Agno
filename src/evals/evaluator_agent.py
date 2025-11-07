5f80803cde593ce5d356e6fe0bd0182a156a59d2
StefKas1 <stefan.kasperzack@gmail.com>
2025-11-07 08:47:57 +0100

    Added evaluator agent.

---
+++ src/evals/evaluator_agent.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from agno.agent import Agent
+from agno.eval.accuracy import AccuracyAgentResponse, AccuracyEval, AccuracyResult
+from agno.models.openai import OpenAIChat
+from agno.tools.calculator import CalculatorTools
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Setup your evaluator Agent
+evaluator_agent = Agent(
+    model=OpenAIChat(id="gpt-4o"),
+    output_schema=AccuracyAgentResponse,  # We want the evaluator agent to return an AccuracyAgentResponse
+    # You can provide any additional evaluator instructions here:
+    # instructions="",
+)
+
+evaluation = AccuracyEval(
+    model=OpenAIChat(id="o4-mini"),
+    agent=Agent(model=OpenAIChat(id="gpt-4o"), tools=[CalculatorTools()]),
+    input="What is 10*5 then to the power of 2? do it step by step",
+    expected_output="2500",
+    # Use your evaluator Agent
+    evaluator_agent=evaluator_agent,
+    # Further adjusting the guidelines
+    additional_guidelines="Agent output should include the steps and the final answer.",
+)
+
+result: Optional[AccuracyResult] = evaluation.run(print_results=True)
+assert result is not None and result.avg_score >= 8
