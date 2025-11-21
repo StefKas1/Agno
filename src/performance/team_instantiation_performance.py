c414aa8da81ab3ffa08310b5043d0cd03fce084b
StefKas1 <stefan.kasperzack@gmail.com>
2025-11-21 09:27:25 +0100

    Added team instantiation performance.

---
+++ src/performance/team_instantiation_performance.py
@@ -0,0 +1,21 @@
+from agno.agent import Agent
+from agno.eval.performance import PerformanceEval
+from agno.models.openai import OpenAIChat
+from agno.team import Team
+from dotenv import load_dotenv
+
+load_dotenv()
+
+team_member = Agent(model=OpenAIChat(id="gpt-4o"))
+
+
+def instantiate_team():
+    return Team(members=[team_member])
+
+
+instantiation_perf = PerformanceEval(
+    name="Instantiation Performance Team", func=instantiate_team, num_iterations=1000
+)
+
+if __name__ == "__main__":
+    instantiation_perf.run(print_results=True, print_summary=True)
