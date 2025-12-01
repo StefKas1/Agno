986b2dd623f92b4f2eea5ccae031d34383511f09
StefKas1 <stefan.kasperzack@gmail.com>
2025-12-01 08:19:42 +0100

    Added telemetry.

---
+++ src/agents/telemetry.py
@@ -0,0 +1,15 @@
+from agno.agent import Agent
+from agno.models.openai import OpenAIChat
+from agno.tools.yfinance import YFinanceTools
+from dotenv import load_dotenv
+
+agent = Agent(
+    model=OpenAIChat(id="gpt-4o"),
+    # https://docs.agno.com/basics/telemetry
+    telemetry=False,  # Disables telemetry
+)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    agent.print_response("What is the stock price of Apple?")
